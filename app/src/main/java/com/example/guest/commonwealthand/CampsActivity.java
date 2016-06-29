19347aec82b15d749550e154e0c9275f38a04f0c
Blake <blakebless7@gmail.com>
2016-06-29 14:52:07 -0700

    contain a listview

--- app/src/main/java/com/example/guest/commonwealthand/CampsActivity.java
+++ app/src/main/java/com/example/guest/commonwealthand/CampsActivity.java
@@ -2,12 +2,27 @@ package com.example.guest.commonwealthand;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class CampsActivity extends AppCompatActivity {
+    @Bind(R.id.listView) ListView mListView;
+    private String[] camps = new String[] {"Summer Skate Camp Session 1 6/20 - 6/23 Sold out! $295",
+            "Summer Skate Camp Session 2 6/27 - 6/30 Sold out! $295"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camps);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, camps);
+        mListView.setAdapter(adapter);
+
+
     }
 }
